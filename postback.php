b8a37d4a1db2c6a46d385c7211ea24ddef84f48b
Nightscrawl <52441295+Nightscrawl@users.noreply.github.com>
2021-01-05 22:42:12 -0800

    w1d1

---
+++ postback.php
@@ -0,0 +1,20 @@
+<?php 
+// postback.php
+
+
+if( isset($_POST['FirstName']) ) { // show data
+
+    echo $_POST['FirstName'];
+
+} else { // show the form
+
+    echo '
+
+        <form action="" method="POST">
+            <p>First Name: <input type="text" name="FirstName" /></p>
+            <input type="submit" />
+        </form>    
+    
+    ';
+
+}
