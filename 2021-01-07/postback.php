52ab5383a2444be0034a24f8408b17ae067d56e1
Nightscrawl <52441295+Nightscrawl@users.noreply.github.com>
2021-01-07 15:59:23 -0800

    postback2 process
    initial

--- 2021-01-07/postback.php
+++ 2021-01-07/postback.php
@@ -1,19 +1,19 @@
-<?php 
-// postback.php
+<?php
+// postback.php -- v2
 
 
 if( isset($_POST['FirstName']) ) { // show data
 
     echo $_POST['FirstName'];
 
-} else { // show the form
+} else { // show form
 
     echo '
 
-        <form action="" method="POST">
+        <form action="" method="POST">        
             <p>First Name: <input type="text" name="FirstName" /></p>
-            <input type="submit" />
-        </form>    
+            <p><input type="submit" value="Submit" /></p>
+        </form>
     
     ';
 
