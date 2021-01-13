35d6cdb291f0c3fdc3c8f7cfac134265b40a1f4c
Nightscrawl <52441295+Nightscrawl@users.noreply.github.com>
2021-01-12 16:55:56 -0800

    constructor

--- 2021-01-12/classes.php
+++ 2021-01-12/classes.php
@@ -4,14 +4,28 @@
 
 class Car {
 
-    public $color = '';
     public $make = '';
     public $model = '';
+    public $color = '';
     public $mileage = 0;  // bc not a string, starts at 0
+
+    public function __construct($make, $model, $color, $mileage) {  // constructor
+
+        // $this->property = object
+        $this->make = $make;
+        $this->model = $model;
+        $this->color = $color;
+        $this->mileage = $mileage;
+
+    }
 
 }
 
 
-$myCar = new Car();
+$myCar = new Car("Toyota", "Prius", "Blue", 40);
 
-echo $myCar->mileage;  // -> like dot syntax
+// echo $myCar->mileage;  // -> like dot syntax
+
+echo '<pre>';
+var_dump($myCar);
+echo '</pre>';
