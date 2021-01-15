22dcd18e3acdacbe1715f3a2aef33b0c7cdfafa8
Nightscrawl <52441295+Nightscrawl@users.noreply.github.com>
2021-01-14 16:09:47 -0800

    average mpg of the cars

--- 2021-01-12/classes2.php
+++ 2021-01-12/classes2.php
@@ -27,10 +27,20 @@ $cars[] = new Car("Toyota", "Prius", "blue", 100);
 $cars[] = new Car("Hummer", "H3", "yellow", 5);
 $cars[] = new Car("Ford", "Mustang", "red", 80);
 
+$total = 0;
+
 // iterates thru array and shows each item
 foreach($cars as $myCar) {
 
     echo "<p>My car is a $myCar->color $myCar->make $myCar->model. It gets $myCar->mileage mpg!</p>";
+    $total += $myCar->mileage;
 
 }
 // My car is a Blue Toyota Prius. It gets 100 mpg!
+
+$carCount = count($cars);
+// $average = $total/count($cars);
+$average = $total/$carCount;
+$average = number_format($average, 2);
+
+echo "<p>There are $carCount cars, and the average mpg is $average.</p>";
